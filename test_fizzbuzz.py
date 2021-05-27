bf244cdb3576fca11a1c31559af2e7e2322d9796
BobbySinclusto <allencbenjamin@gmail.com>
2021-05-27 00:28:47 -0700

    Wrote second test, testing that all lines of output that should contain only numbers are correct

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -6,3 +6,14 @@ def test_first_line(capsys):
     # We'll get an array that contains each line in the output (the -1 is to deal with the trailing newline)
     output_lines = capsys.readouterr().out.split('\n')[:-1]
     assert output_lines[0] == '1'
+
+# Make sure that lines that just contain numbers are correct
+def test_non_special_lines(capsys):
+    fizzbuzz.fizzbuzz()
+    # Capture stdout so that we can check for things in tests
+    # We'll get an array that contains each line in the output (the -1 is to deal with the trailing newline)
+    output_lines = capsys.readouterr().out.split('\n')[:-1]
+    
+    for i in range(1, 101):
+        if i % 3 != 0 and i % 5 != 0:
+            assert output_lines[i - 1] == str(i)
