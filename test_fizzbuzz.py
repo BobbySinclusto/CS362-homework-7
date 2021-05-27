ede5108fe4e4e66f01803e45394c4235e9d4db7d
BobbySinclusto <allencbenjamin@gmail.com>
2021-05-27 00:40:13 -0700

    Wrote fifth test, testing that lines that should show FizzBuzz actually show FizzBuzz

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -39,3 +39,14 @@ def test_buzz(capsys):
     for i in range(1, 101):
         if i % 5 == 0 and i % 3 != 0:
             assert output_lines[i - 1] == 'Buzz'
+
+# Make sure that lines that are divisible by 5 and 3 are correct
+def test_fizz_and_buzz(capsys):
+    fizzbuzz.fizzbuzz()
+    # Capture stdout so that we can check for things in tests
+    # We'll get an array that contains each line in the output (the -1 is to deal with the trailing newline)
+    output_lines = capsys.readouterr().out.split('\n')[:-1]
+    
+    for i in range(1, 101):
+        if i % 5 == 0 and i % 3 == 0:
+            assert output_lines[i - 1] == 'FizzBuzz'
