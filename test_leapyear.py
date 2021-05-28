76233b98d39add78505b63e8f91fcc5f4a589642
BobbySinclusto <allencbenjamin@gmail.com>
2021-05-27 22:37:08 -0700

    Wrote test for leapyear returning false when the year is not a leap year

--- test_leapyear.py
+++ test_leapyear.py
@@ -6,3 +6,10 @@ def test_div_4():
     assert leapyear.is_leapyear(4) == True
     assert leapyear.is_leapyear(2020) == True
     assert leapyear.is_leapyear(1924) == True
+
+# Test to make sure thzt the program correctly detects that years not divisible by 4
+# are not leap years
+def test_not_div_4():
+    assert leapyear.is_leapyear(3) == False
+    assert leapyear.is_leapyear(1999) == False
+    assert leapyear.is_leapyear(2001) == False
